1e2ac4af35fd2dde0fe9409df08eba68bfd0dedb
igorsoares <igor.hscuculha@gmail.com>
2021-12-22 09:13:23 -0300

    Changing to axios

--- dist/index.js
+++ dist/index.js
@@ -11486,24 +11486,24 @@ const { context } = __nccwpck_require__(5438)
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 const octokit = github.getOctokit(GITHUB_TOKEN);
-  
+url_media = undefined    
 const { pull_request } = context.payload;
   
 async function run() {
     randomPos=Math.floor(Math.random() * 10)
     const url=`https://g.tenor.com/v1/search?q=thank+you+leonardo+dicaprio&pos=${randomPos}&limit=1&media_filter=tinygif&contentfilter=high&key=${TENOR_TOKEN}`
     console.log(url)
-    url_media = undefined    
+    
     msg='Obrigado pelo feedback. Avaliaremos assim que possível.'
     axios.get(url).then(function (response) { 
         var x = response.data.results
         url_media = x[0].media[0].tinygif.url
         console.log("URL_MEDIA :" + url_media)
-        console.log(x)
+        
     }).catch(function (error) { 
         console.log(error)
     })
-    
+    console.log("URL_MEDIA :" + url_media)
   
 
     await octokit.rest.issues.createComment({
