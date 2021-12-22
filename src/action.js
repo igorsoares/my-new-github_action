e39d69c56015d09f7213ce87dd4b30d561e720dd
igorsoares <igor.hscuculha@gmail.com>
2021-12-22 08:19:31 -0300

    Intro

---
+++ src/action.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const GITHUB_TOKEN=core.getInput('GITHUB_TOKEN')
+const octokit = github.getOctokit(GITHUB_TOKEN)
+const { context = {} } = github
+const { pull_request } = context.payload
+async function run() { 
+    octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number:pull_request.issue_number,
+        body:'Testing'
+    })
+}
+
+run()
