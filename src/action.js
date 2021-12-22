a57aa2f9baa0f6b40ee571f77f0fe97126ae9290
igorsoares <igor.hscuculha@gmail.com>
2021-12-22 08:45:54 -0300

    Adding gifs

--- src/action.js
+++ src/action.js
@@ -3,11 +3,18 @@ const github = require('@actions/github');
 const myfetch=require('node-fetch')
 const { context } = require('@actions/github')
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 const octokit = github.getOctokit(GITHUB_TOKEN);
   
 const { pull_request } = context.payload;
   
 async function run() {
+    randomPos=Math.floor(Math.random() * 1000)
+    const url=`https://g.tenor.com/v1/search?q=thank+you+leonardo+dicaprio&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`
+    resp = await myfetch(url)
+    const respJson = resp.json()
+    console.log(respJson)
+
   await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
