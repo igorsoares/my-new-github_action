903a2d91ab7b3016051bf48fa3a707ff36b0164a
igorsoares <igor.hscuculha@gmail.com>
2021-12-22 14:51:51 -0300

    Artia comment integration

--- src/testAxios.js
+++ src/testAxios.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
 randomPos = Math.floor(Math.random() * 10)
-const url=`https://g.tenor.com/v1/search?q=thank+you+leonardo+dicaprio&pos=${randomPos}&limit=1&media_filter=tinygif&contentfilter=high&key=0ARWZ9AIEKM1`
+const url=`https://g.tenor.com/v1/search?q=thank+you+leonardo+dicaprio&pos=${randomPos}&limit=1&media_filter=tinygif&contentfilter=high&key=${TENOR_TOKEN}`
     
 axios.get(url).then(function (response) { 
     var x = response.data.results
     var media=x[0].media[0].tinygif.url
     console.log(media)
-    
+
 }).catch(function (error) { 
     console.log(error)
 })
